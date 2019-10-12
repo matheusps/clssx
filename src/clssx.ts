7d92e6e564bd4ee0e7d4447c7e5eda67b8cde666
matheusps <cout.matheusps@gmail.com>
2019-10-12 11:40:45 -0300

    Added supoort for multiple params

--- src/clssx.ts
+++ src/clssx.ts
@@ -1,13 +1,26 @@
-function clssx(params: cls): string {
-  if (typeof params === 'string' || typeof params === 'number') return `${params}` || ''
+const SPACE = ' '
 
-  const SPACE = ' '
-  const isTruthy = (param: string) => params.hasOwnProperty(param) && params[param]
+const concat = (a: string, b: string): string => (!!a && !!b ? a + SPACE + b : a || b)
 
-  return Object.keys(params)
-    .filter(isTruthy)
-    .join(SPACE)
+const reducer = (state: string, param: cls): string => {
+  switch (typeof param) {
+    case 'string':
+      return concat(state, param)
+    case 'number':
+      return concat(state, `${param}`)
+    default: {
+      const isTruthy = (property: string) => param.hasOwnProperty(property) && param[property]
+      return concat(
+        state,
+        Object.keys(param)
+          .filter(isTruthy)
+          .join(SPACE)
+      )
+    }
+  }
 }
+
+const clssx = (...params: Array<cls>): string => params.reduce(reducer, '')
 
 type cls = string | number | { [key: string]: boolean }
 
--- test/clssx.test.ts
+++ test/clssx.test.ts
@@ -1,22 +1,47 @@
 import clssx from '../src/clssx'
 
-describe("Clssx", () => {
-  it("Works with strings", () => {
+describe('Clssx', () => {
+  it('Works with strings', () => {
     expect(clssx('')).toBe('')
-    expect(clssx('flex')).toBe('flex')
+    expect(clssx('flex', 'bold', 't-small')).toBe('flex bold t-small')
     expect(clssx('flex w-100 pa5')).toBe('flex w-100 pa5')
     expect(clssx('flex w-100 pa5 m20 text-center')).toBe('flex w-100 pa5 m20 text-center')
   })
 
-  it("Works with numbers", () => {
+  it('Works with numbers', () => {
     expect(clssx(0)).toBe('0')
-    expect(clssx(1)).toBe('1')
+    expect(clssx(1, 10, 20, 40)).toBe('1 10 20 40')
     expect(clssx(200)).toBe('200')
   })
 
-  it("Works with maps", () => {
-    expect(clssx({ 'flex': true, 'w-100 text-center': true})).toBe('flex w-100 text-center')
-    expect(clssx({ 'flex': false, 'w-100 text-center': true})).toBe('w-100 text-center')
-    expect(clssx({ 'flex': false, 'w-100 text-center': false})).toBe('')
+  it('Works with maps', () => {
+    expect(clssx({ flex: true, 'w-100 text-center': true })).toBe('flex w-100 text-center')
+    expect(clssx({ flex: false, 'w-100 text-center': true })).toBe('w-100 text-center')
+    expect(clssx({ flex: false, 'w-100 text-center': false })).toBe('')
+    expect(
+      clssx({ flex: false, 'w-100 text-center': false }, { 't-small': true, pa4: false })
+    ).toBe('t-small')
+    expect(clssx({ flex: true, 'w-100 text-center': false }, { 't-small': true, pa4: false })).toBe(
+      'flex t-small'
+    )
+    expect(clssx({ flex: true, 'w-100 text-center': true }, { 't-small': true, pa4: false })).toBe(
+      'flex w-100 text-center t-small'
+    )
+    expect(clssx({ flex: true, 'w-100 text-center': true }, { 't-small': true, pa4: true })).toBe(
+      'flex w-100 text-center t-small pa4'
+    )
+  })
+
+  it('Works with mixed types', () => {
+    expect(clssx('flex', 23, { 'w-100 text-center': true })).toBe('flex 23 w-100 text-center')
+    expect(
+      clssx(
+        'flex',
+        23,
+        { 'w-100 text-center': true },
+        { 't-small': true, pa4: true },
+        { xs: false }
+      )
+    ).toBe('flex 23 w-100 text-center t-small pa4')
   })
 })
