397d1e5a0dfe08362821ef9de5600ea487655349
Parth <parth.shah@sprinklr.com>
2019-10-20 23:25:09 +0530

    Accept null or undefined on ssx function

--- test/ssx.test.ts
+++ test/ssx.test.ts
@@ -1,6 +1,22 @@
 import ssx from '../src/ssx'
 
 describe('ssx', () => {
+  it('works with undefined', () => {
+    expect(ssx(undefined)).toEqual({})
+    expect(ssx({ color: '#000' }, { alignContent: 'center' }, undefined)).toEqual({
+      color: '#000',
+      alignContent: 'center'
+    })
+  });
+
+  it('works with null', () => {
+    expect(ssx(null)).toEqual({})
+    expect(ssx({ color: '#000' }, { alignContent: 'center' }, null)).toEqual({
+      color: '#000',
+      alignContent: 'center'
+    })
+  });
+
   it('Works with objects', () => {
     expect(ssx({})).toEqual({})
     expect(ssx({ color: '#000' }, { alignContent: 'center' })).toEqual({
@@ -20,10 +36,10 @@ describe('ssx', () => {
     })
   })
 
-  it('Works with arrays and objects', () => {
+  it('Works with undefined, null, arrays and objects', () => {
     expect(ssx({}, [])).toEqual({})
     expect(
-      ssx({ display: 'flex' }, [{ color: '#000' }, true], [{ alignContent: 'center' }, true])
+      ssx(null, { display: 'flex' }, [{ color: '#000' }, true], undefined, [{ alignContent: 'center' }, true])
     ).toEqual({
       display: 'flex',
       color: '#000',
@@ -31,6 +47,8 @@ describe('ssx', () => {
     })
     expect(
       ssx(
+        null,
+        undefined,
         { display: 'inline-block' },
         [{ color: '#000' }, true],
         [{ alignContent: 'center' }, false]
